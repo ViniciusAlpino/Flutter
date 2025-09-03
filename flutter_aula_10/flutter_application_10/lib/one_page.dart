ebf0daa82a0d098c591293efd67125245c2f7ff1
ViniciusAlpino <redondovinicius@gmail.com>
2025-09-03 18:03:04 -0300

    Gerencia de estado Nativo

--- flutter_aula_10/flutter_application_10/lib/one_page.dart
+++ flutter_aula_10/flutter_application_10/lib/one_page.dart
@@ -1,8 +1,23 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_10/custom_button_widget.dart';
 
-class OnePage extends StatelessWidget {
+class OnePage extends StatefulWidget {
   const OnePage({super.key});
+
+  @override
+  State<OnePage> createState() => _OnePageState();
+}
+
+class _OnePageState extends State<OnePage> {
+  ValueNotifier<int> valorAleatorio = ValueNotifier<int>(0);
+
+  random() async {
+    for (int i = 0; i < 10; i++) {
+      await Future.delayed(const Duration(seconds: 1));
+      valorAleatorio.value = Random().nextInt(10000);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,15 +27,26 @@ class OnePage extends StatelessWidget {
         title: Text('Page 1'),
         titleTextStyle: TextStyle(color: Colors.white, fontSize: 25),
       ),
-      backgroundColor: Colors.purple.shade300,
       body: Center(
-        child: CustomButtonWidget(
-          disabled: false,
-          onPressed: () {},
-          title: 'Custom BTN',
-          titleSize: 20,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomButtonWidget(
+              disabled: false,
+              onPressed: () => random(),
+              title: 'Custom BTN',
+              titleSize: 20,
+            ),
+            SizedBox(height: 10,),
+            ValueListenableBuilder(
+              valueListenable: valorAleatorio,
+              builder: (_, value, __) => Text('Valor eh: $value',
+               style: TextStyle(fontSize: 18),
+               ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
