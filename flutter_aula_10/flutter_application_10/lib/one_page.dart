a3138aff7ba59fda48ecb4d101b99ed0ffbc19b5
ViniciusAlpino <redondovinicius@gmail.com>
2025-09-09 17:39:41 -0300

    chamada de API

--- flutter_aula_10/flutter_application_10/lib/one_page.dart
+++ flutter_aula_10/flutter_application_10/lib/one_page.dart
@@ -1,4 +1,5 @@
-import 'dart:math';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_application_10/custom_button_widget.dart';
 
@@ -10,12 +11,22 @@ class OnePage extends StatefulWidget {
 }
 
 class _OnePageState extends State<OnePage> {
-  ValueNotifier<int> valorAleatorio = ValueNotifier<int>(0);
+  ValueNotifier<List<Post>> posts = ValueNotifier<List<Post>>([]);
+  ValueNotifier<bool> inLoader = ValueNotifier<bool>(false);
 
-  random() async {
-    for (int i = 0; i < 10; i++) {
-      await Future.delayed(const Duration(seconds: 1));
-      valorAleatorio.value = Random().nextInt(10000);
+  callAPI() async {
+    var client = http.Client();
+    try {
+      inLoader.value = true;
+      var response = await client.get(
+        Uri.parse("https://jsonplaceholder.typicode.com/posts"),
+      );
+      var decodedResponse = jsonDecode(response.body) as List;
+      posts.value = decodedResponse.map((e) => Post.fromJson(e)).toList();
+      await Future.delayed(Duration(seconds: 2));
+    } finally {
+      client.close();
+      inLoader.value = false;
     }
   }
 
@@ -28,25 +39,50 @@ class _OnePageState extends State<OnePage> {
         titleTextStyle: TextStyle(color: Colors.white, fontSize: 25),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomButtonWidget(
-              disabled: false,
-              onPressed: () => random(),
-              title: 'Custom BTN',
-              titleSize: 20,
-            ),
-            SizedBox(height: 10,),
-            ValueListenableBuilder(
-              valueListenable: valorAleatorio,
-              builder: (_, value, __) => Text('Valor eh: $value',
-               style: TextStyle(fontSize: 18),
-               ),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomButtonWidget(
+                disabled: false,
+                onPressed: () => callAPI(),
+                title: 'Custom BTN',
+                titleSize: 20,
+              ),
+              SizedBox(height: 10),
+              AnimatedBuilder(
+                animation: Listenable.merge([posts, inLoader]),
+                builder: (_, __) => inLoader.value
+                    ? CircularProgressIndicator()
+                    : ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: posts.value.length,
+                        itemBuilder: (_, idx) =>
+                            ListTile(title: Text(posts.value[idx].title)),
+                      ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+}
+
+class Post {
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post(this.userId, this.id, this.title, this.body);
+
+  factory Post.fromJson(Map json) {
+    return Post(json['userId'], json['id'], json['title'], json['body']);
+  }
+  @override
+  String toString() {
+    return 'id $id';
   }
 }
